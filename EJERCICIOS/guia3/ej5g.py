8c9ec7629c8190f1e1f3e0e1e9b6effa9a27c185
mateocastro1267 <111094194+mateocastro1267@users.noreply.github.com>
2024-04-19 11:11:00 -0300

    act ej5

--- EJERCICIOS/guia3/ej5g.py
+++ EJERCICIOS/guia3/ej5g.py
@@ -1,5 +1,5 @@
 try:
-    with open('esfuerzo_padre.txt', 'w') as archivo:
+    with open('ej1G3.txt', 'w') as archivo:
         archivo.write(123)
 except ValueError:
     print("No se puede escribir en el archivo")
